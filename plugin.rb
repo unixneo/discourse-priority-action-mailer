f0b1d5dce5c230df0ea5fc194c5f3e8b5f702e7a
Tim Bass <neo@unix.com>
2020-11-23 19:30:49 +0700

    DEV: Initial draft commit.

---
+++ plugin.rb
@@ -0,0 +1,36 @@
+# name: discourse-priority-action-mailer
+# about: plugin to add priority smtp_settings to action mailer
+# version: 0.0.1
+# date: 23 Nov 2020
+# authors: Neo
+# url: https://community.unix.com/t/creating-higher-priority-smtp-settings-in-discourse-software-mailers-a-future-plugin-idea/380865
+
+register_asset "stylesheets/common/container-names.scss"
+
+HIGH_PRIORITY_SMTP_SETTINGS = {
+  :address => "my.email.server.com",
+  :port => 587,
+  :user_name => "my_email_address",
+  :password => "my_email_password",
+  :authentication => "plain",
+  :enable_starttls_auto => true,
+}
+after_initialize do
+  after_initialize do
+    AdminConfirmationMailer.class_eval do
+      before_action :high_priority_smtp_settings
+
+      def high_priority_smtp_settings
+        self.smtp_settings = HIGH_PRIORITY_SMTP_SETTINGS
+      end
+    end
+
+    UserNotifications.class_eval do
+      before_action :high_priority_smtp_settings, only: [:digest]
+
+      def high_priority_smtp_settings
+        self.smtp_settings = HIGH_PRIORITY_SMTP_SETTINGS
+      end
+    end
+  end
+end
