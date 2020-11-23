e0b41768b9893af98568bac51e9e9d1b5ab13f99
Tim Bass <neo@unix.com>
2020-11-23 21:54:31 +0700

    BUMP: Version

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-priority-action-mailer
 # about: plugin to add priority smtp_settings to action mailer
-# version: 0.0.5
+# version: 0.0.6
 # date: 23 Nov 2020
 # authors: Neo
 # url: https://community.unix.com/t/creating-higher-priority-smtp-settings-in-discourse-software-mailers-a-future-plugin-idea/380865
