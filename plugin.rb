cbd0f9e567dbeb3e726ea798cd2741efbddee59a
Tim Bass <neo@unix.com>
2020-11-24 17:47:07 +0700

    DEV: change method to:  updade_smtp_settings

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-priority-action-mailer
 # about: plugin to add priority smtp_settings to action mailer
-# version: 0.0.13
+# version: 0.0.15
 # date: 24 Nov 2020
 # authors: Neo
 # url: https://community.unix.com/t/creating-higher-priority-smtp-settings-in-discourse-software-mailers-a-future-plugin-idea/380865
@@ -18,9 +18,9 @@ NEODEBUG = true
 
 after_initialize do
   AdminConfirmationMailer.instance_eval do
-    before_action :high_priority_smtp_settings
+    before_action :update_smtp_settings
 
-    def high_priority_smtp_settings
+    def update_smtp_settings
       puts ">>>>> NEO AdminConfirmationMailer GlobalSetting.smtp_password_priority #{GlobalSetting.smtp_password_priority}" if NEODEBUG
       if GlobalSetting.smtp_password_priority.present?
         puts ">>>>> NEO AdminConfirmationMailer PRIORITY_SMTP_SETTINGS #{PRIORITY_SMTP_SETTINGS}" if NEODEBUG
@@ -31,9 +31,9 @@ after_initialize do
   end
 
   UserNotifications.instance_eval do
-    before_action :high_priority_smtp_settings, only: [:email_login]
+    before_action :update_smtp_settings, only: [:email_login]
 
-    def high_priority_smtp_settings
+    def update_smtp_settings
       puts ">>>>> NEO UserNotifications GlobalSetting.smtp_password_priority #{GlobalSetting.smtp_password_priority}" if NEODEBUG
       if GlobalSetting.smtp_password_priority.present?
         puts ">>>>> NEO UserNotifications PRIORITY_SMTP_SETTINGS #{PRIORITY_SMTP_SETTINGS}" if NEODEBUG
