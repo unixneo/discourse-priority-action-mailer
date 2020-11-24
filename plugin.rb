1da4d5c6e9405bf13421c2f2647a8fb08da50484
Tim Bass <neo@unix.com>
2020-11-24 17:26:36 +0700

    DEV: Change name of PRIORITY_SMTP_SETTINGS constant

--- plugin.rb
+++ plugin.rb
@@ -1,11 +1,11 @@
 # name: discourse-priority-action-mailer
 # about: plugin to add priority smtp_settings to action mailer
-# version: 0.0.11
+# version: 0.0.12
 # date: 24 Nov 2020
 # authors: Neo
 # url: https://community.unix.com/t/creating-higher-priority-smtp-settings-in-discourse-software-mailers-a-future-plugin-idea/380865
 
-HIGH_PRIORITY_SMTP_SETTINGS = {
+PRIORITY_SMTP_SETTINGS = {
   address: GlobalSetting.smtp_address_priority,
   port: GlobalSetting.smtp_port_priority,
   user_name: GlobalSetting.smtp_user_name_priority,
@@ -25,8 +25,8 @@ after_initialize do
     def high_priority_smtp_settings
       puts ">>>>> NEO AdminConfirmationMailer GlobalSetting.smtp_password_priority #{GlobalSetting.smtp_password_priority}" if NEODEBUG
       if GlobalSetting.smtp_password_priority.present?
-        puts ">>>>> NEO AdminConfirmationMailer HIGH_PRIORITY_SMTP_SETTINGS #{HIGH_PRIORITY_SMTP_SETTINGS}" if NEODEBUG
-        AdminConfirmationMailer.smtp_settings = HIGH_PRIORITY_SMTP_SETTINGS
+        puts ">>>>> NEO AdminConfirmationMailer PRIORITY_SMTP_SETTINGS #{PRIORITY_SMTP_SETTINGS}" if NEODEBUG
+        AdminConfirmationMailer.smtp_settings = PRIORITY_SMTP_SETTINGS
         puts ">>>>> NEO AdminConfirmationMailer AdminConfirmationMailer.smtp_settings #{AdminConfirmationMailer.smtp_settings}" if NEODEBUG
       end
     end
@@ -40,9 +40,9 @@ after_initialize do
     def high_priority_smtp_settings
       puts ">>>>> NEO UserNotifications GlobalSetting.smtp_password_priority #{GlobalSetting.smtp_password_priority}" if NEODEBUG
       if GlobalSetting.smtp_password_priority.present?
-        puts ">>>>> NEO UserNotifications HIGH_PRIORITY_SMTP_SETTINGS #{HIGH_PRIORITY_SMTP_SETTINGS}" if NEODEBUG
-        UserNotifications.smtp_settings = HIGH_PRIORITY_SMTP_SETTINGS
-        puts ">>>>> NEO UserNotifications UserNotifications.smtp_setting #{UserNotifications.smtp_settings}" if NEODEBUG
+        puts ">>>>> NEO UserNotifications PRIORITY_SMTP_SETTINGS #{PRIORITY_SMTP_SETTINGS}" if NEODEBUG
+        UserNotifications.smtp_settings = PRIORITY_SMTP_SETTINGS
+        puts ">>>>> NEO UserNotifications UserNotifications.smtp_settings #{UserNotifications.smtp_settings}" if NEODEBUG
       end
     end
   end
