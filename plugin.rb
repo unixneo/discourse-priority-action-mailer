bd0228a6e0d5ae9c400af40e5a6d1e64f5daf8f6
Tim Bass <neo@unix.com>
2020-11-24 16:41:39 +0700

    DEV:  DEBUG

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-priority-action-mailer
 # about: plugin to add priority smtp_settings to action mailer
-# version: 0.0.7
+# version: 0.0.8
 # date: 23 Nov 2020
 # authors: Neo
 # url: https://community.unix.com/t/creating-higher-priority-smtp-settings-in-discourse-software-mailers-a-future-plugin-idea/380865
@@ -10,9 +10,11 @@ HIGH_PRIORITY_SMTP_SETTINGS = {
   port: GlobalSetting.smtp_port_priority,
   user_name: GlobalSetting.smtp_user_name_priority,
   password: GlobalSetting.smtp_password_priority,
-  authentication: "plain",
-  enable_starttls_auto: true,
+  authentication: "plain": GlobalSetting.smtp_authentication,
+  enable_starttls_auto: GlobalSetting.smtp_enable_start_tls,
 }
+
+NEODEBUG = true
 
 after_initialize do
   AdminConfirmationMailer.class_eval do
@@ -21,8 +23,11 @@ after_initialize do
     private
 
     def high_priority_smtp_settings
+      puts ">>>>> NEO AdminConfirmationMailer GlobalSetting.smtp_password_priority #{GlobalSetting.smtp_password_priority}" if NEODEBUG
       if GlobalSetting.smtp_password_priority.present?
+        puts ">>>>> NEO AdminConfirmationMailer HIGH_PRIORITY_SMTP_SETTINGS #{HIGH_PRIORITY_SMTP_SETTINGS}" if NEODEBUG
         AdminConfirmationMailer.smtp_settings = HIGH_PRIORITY_SMTP_SETTINGS
+        puts ">>>>> NEO AdminConfirmationMailer AdminConfirmationMailer.smtp_settings #{AdminConfirmationMailer.smtp_settings}" if NEODEBUG
       end
     end
   end
@@ -33,8 +38,11 @@ after_initialize do
     private
 
     def high_priority_smtp_settings
+      puts ">>>>> NEO UserNotifications GlobalSetting.smtp_password_priority #{GlobalSetting.smtp_password_priority}" if NEODEBUG
       if GlobalSetting.smtp_password_priority.present?
+        puts ">>>>> NEO UserNotifications HIGH_PRIORITY_SMTP_SETTINGS #{HIGH_PRIORITY_SMTP_SETTINGS}" if NEODEBUG
         UserNotifications.smtp_settings = HIGH_PRIORITY_SMTP_SETTINGS
+        puts ">>>>> NEO UserNotifications UserNotifications.smtp_setting #{UserNotifications.smtp_settingS}" if NEODEBUG
       end
     end
   end
